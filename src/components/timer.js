155866457c7e85787265eaaf51e42c2de60444b6
cturgeon <casey.turgeon@gmail.com>
2022-07-19 14:11:03 -0500

    add: preliminary timer and button

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
+
+import { Box, Button, Card, NumberInput } from "@mantine/core";
+
 export default function TimerComponent(props) {
+  const [value, setValue] = useState(25);
+  const [startTimer, setStartTimer] = useState(false);
+  useEffect(() => {
+    if (value > 0 && startTimer) {
+      setTimeout(() => {
+        setValue(value - 1);
+      }, 1000);
+    }
+    if (value === 0 && startTimer) {
+      setStartTimer(false);
+    }
+  }, [value, startTimer]);
+
+  const timerButtonHandler = () => {
+    setStartTimer(true);
+  };
   return (
     <>
-      <h1>Timer</h1>
+      <Box sx={{ width: 100 }} mx="auto">
+        <Card>
+          <NumberInput
+            hideControls
+            value={value}
+            onChange={(val) => setValue(val)}
+            min={0}
+          />
+          <Button onClick={timerButtonHandler}>Click</Button>
+        </Card>
+      </Box>
     </>
   );
 }
