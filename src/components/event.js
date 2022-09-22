c45d56e2e1656c97eaf83bd58bc5f1b41dd48908
mitchellciupak <ciupak@purdue.edu>
2022-09-22 20:07:13 +0000

    event component bugfix, state update and event object creation

--- src/components/event.js
+++ src/components/event.js
@@ -3,39 +3,42 @@ import { Box, Card, TextInput, Button } from '@mantine/core';
 
 const minutes = 30;
 
-const event = {
-    'summary': 'Riding a Bike',
-    'location': '402 At My Desk Blvd, Moon, Dark Side',
-    'description': 'I plan to ride my bike',
-    'start': {
-        'dateTime': new Date().toISOString(),
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
-    },
-    'end': {
-        'dateTime': (new Date(new Date().getTime() + (minutes * 60 * 1000))).toISOString(),
-        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
-    },
-    // 'recurrence': [
-    //     'RRULE:FREQ=DAILY;COUNT=2'
-    // ],
-    // 'attendees': [
-    //     { 'email': 'lpage@example.com' },
-    //     { 'email': 'sbrin@example.com' },
-    // ],
-    // 'reminders': {
-    //     'useDefault': false,
-    //     'overrides': [
-    //         { 'method': 'email', 'minutes': 24 * 60 },
-    //         { 'method': 'popup', 'minutes': 10 },
-    //     ],
-    // },
-};
+
 
 export default function EventComponent(props) {
-    const [eventSummary, setEventSummaryValue] = useState(event.summary);
+    const [summary, setSummary] = useState("write your event summary here.");
 
-    const submitButtonHandler = () => {
-        alert('A Event was submitted to Google Calendar: ' + eventSummary);
+    const startButtonHandler = () => {
+
+        const event = {
+            'summary': summary,
+            'location': '402 At My Desk Blvd, Moon, Dark Side',
+            'description': 'I plan to ride my bike',
+            'start': {
+                'dateTime': new Date().toISOString(),
+                'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
+            },
+            'end': {
+                'dateTime': (new Date(new Date().getTime() + (minutes * 60 * 1000))).toISOString(),
+                'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
+            },
+            // 'recurrence': [
+            //     'RRULE:FREQ=DAILY;COUNT=2'
+            // ],
+            // 'attendees': [
+            //     { 'email': 'lpage@example.com' },
+            //     { 'email': 'sbrin@example.com' },
+            // ],
+            // 'reminders': {
+            //     'useDefault': false,
+            //     'overrides': [
+            //         { 'method': 'email', 'minutes': 24 * 60 },
+            //         { 'method': 'popup', 'minutes': 10 },
+            //     ],
+            // },
+        };
+
+        alert('A Event was submitted to Google Calendar: ' + summary);
     }
 
     return (
@@ -43,14 +46,13 @@ export default function EventComponent(props) {
             <Box sx={{ width: 1000 }} mx="auto">
                 <Card>
                     <TextInput
-                        value={eventSummary}
-                        onChange={(eventSummary) => setEventSummaryValue(eventSummary)}
+                        value={summary}
+                        onChange={event => setSummary(event.target.value)}
                         placeholder="write your event summary here."
                         label="Event Summary"
                         radius="sm"
-                        withAsterisk
                     />
-                    <Button onClick={submitButtonHandler}>Submit</Button>
+                    <Button onClick={startButtonHandler}>Start</Button>
                 </Card>
             </Box>
         </>
