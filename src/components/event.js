d7205d7ac83e78c41f72bb1d03007edfb73cd90d
mitchellciupak <ciupak@purdue.edu>
2022-09-22 00:38:51 +0000

    event componenet: event object: commenting out uneeded fields

--- src/components/event.js
+++ src/components/event.js
@@ -14,20 +14,20 @@ const event = {
         'dateTime': (new Date(new Date().getTime() + (minutes * 60 * 1000))).toISOString(),
         'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone,
     },
-    'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
-    ],
-    'attendees': [
-        { 'email': 'lpage@example.com' },
-        { 'email': 'sbrin@example.com' },
-    ],
-    'reminders': {
-        'useDefault': false,
-        'overrides': [
-            { 'method': 'email', 'minutes': 24 * 60 },
-            { 'method': 'popup', 'minutes': 10 },
-        ],
-    },
+    // 'recurrence': [
+    //     'RRULE:FREQ=DAILY;COUNT=2'
+    // ],
+    // 'attendees': [
+    //     { 'email': 'lpage@example.com' },
+    //     { 'email': 'sbrin@example.com' },
+    // ],
+    // 'reminders': {
+    //     'useDefault': false,
+    //     'overrides': [
+    //         { 'method': 'email', 'minutes': 24 * 60 },
+    //         { 'method': 'popup', 'minutes': 10 },
+    //     ],
+    // },
 };
 
 export default class EventComponent extends React.Component {
