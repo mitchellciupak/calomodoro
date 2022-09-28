c72f7fd3aa945721b2087d5ca8a023f953f24f74
mitchellciupak <ciupak@purdue.edu>
2022-09-28 01:56:10 +0000

    navbar add, index update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import { AppShell, Navbar, Header } from "@mantine/core";
 import TimerComponent from "./components/timer";
 import EventComponent from "./components/event";
 
 function App() {
   return (
     <div className="App">
-      <TimerComponent />
-      <EventComponent />
+      <AppShell
+        padding="md"
+        navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+        header={<Header height={60} p="xs">{/* Header content */}</Header>}
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+      >
+        <TimerComponent />
+        <EventComponent />
+      </AppShell>
     </div>
   );
 }
