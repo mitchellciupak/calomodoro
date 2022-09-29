32e407f6f22e642b5b31f9674a7c5364b3dfd52a
mitchellciupak <ciupak@purdue.edu>
2022-09-29 00:42:41 +0000

    appshel, version added to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,55 @@
 import "./App.css";
-import React from "react";
-import { version } from '../package.json';
-import { AppShell, Header, Footer } from "@mantine/core";
+import React, { useState } from "react";
+import packageJSON from '../package.json';
+import {
+  AppShell,
+  Header,
+  Footer,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
 import TimerComponent from "./components/timer";
 import EventComponent from "./components/event";
 
-function App() {
+export default function AppShellDemo() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
-    <div className="App">
-      <AppShell
-        padding="md"
-        header={<Header height={60} p="xs">{<h1>Calomodoro</h1>}</Header>}
-        styles={(theme) => ({
-          main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
-        })}
-        footer={
-          <Footer height={60} p="md">
-            version
-          </Footer>
-        }
-      >
-        <TimerComponent />
-        <EventComponent />
-      </AppShell>
-    </div>
+    <AppShell
+      styles={{
+        main: {
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      footer={
+        <Footer height={50} p="md">
+          v. {packageJSON.version}
+        </Footer>
+      }
+      header={
+        <Header height={70} p="md">
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+
+            <Text>Calomodoro</Text>
+          </div>
+        </Header>
+      }
+    >
+      <TimerComponent />
+      <EventComponent />
+    </AppShell>
   );
 }
-
-export default App;
